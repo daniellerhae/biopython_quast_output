d910e833b55ac757dd2d4846410c69c9255e3d81
daniellerhae <123119756+daniellerhae@users.noreply.github.com>
2023-03-01 18:35:06 -0600

    Add files via upload
    This is a .py file that can be created in the terminal and run in in the command line using $python3 myquast.py <file.fa> 
    The purpose is to provide an output similar to that of QUAST, summarizing a fasta file and providing the following four outputs: N50 value, total number of contigs, total length of all contigs, and length of the largest contig. 
    In this example the input file is "contigs.fa" - this code can be run on any fasta file format.

---
+++ myquast.py
@@ -0,0 +1,46 @@
+import Bio
+from Bio import SeqIO
+
+max_contig = 0
+contigs_length = []
+sum_length = 0
+
+
+with open("contigs.fa", "r") as contigs_fasta:
+    my_data = SeqIO.parse("contigs.fa", "fasta")
+
+## calculate total length of contigs
+contig_sum = sum(len(record) for record in SeqIO.parse("contigs.fa", "fasta"))
+
+## calculate number of contigs
+contig_count = list(SeqIO.parse("contigs.fa", "fasta"))
+len(contig_count)
+
+## calculate length of longest contig
+for record in SeqIO.parse("contigs.fa", "fasta"):
+    if len(record) > max_contig:
+        max_contig = len(record)
+list_contigs = []
+for record in my_data:
+    list_contigs.append(len(record))
+    list_contigs.sort()
+
+
+N50_thresh = (contig_sum/2)
+N50_value = 0
+N50_list = []
+
+for i in list_contigs:
+    if N50_value + i < N50_thresh:
+        N50_value = N50_value + i
+        N50_list.append(i)
+    elif N50_value >= N50_thresh:
+        break
+
+print('Number of contigs: ', len(contig_count))
+print('Total length: ',contig_sum)
+print('Length of longest contig: ',max_contig)
+print('N50: ' + str(max(N50_list)))
+
+
+exit()
